8e2ff0c080bd169feb98b14a7def04519382afab
Blue Stones <Sergio@bluestonesmortgage.comgit config --global user.email>
2023-10-20 10:39:03 -0400

    okk

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,50 @@
-let myName = "sergio";
-console.log(myName);
+
+/*
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+    const myName = prompt("Please enter your name.");
+    localStorage.setItem("name", myName);
+    myHeading.textContent = `Poker is great, ${myName}`;   
+  }
+  if (!localStorage.getItem("name")) {
+   setUserName();
+  } else {
+    const storedName = localStorage.getItem("name");
+    myHeading.textContent = `Poker is great, ${storedName}`;
+  }
+
+myButton.onclick = () => {
+  setUserName();
+};
+
+*/
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+  let myName = prompt('Please enter your name.');
+  if(!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem('name', myName);
+    myHeading.innerHTML = 'Mozilla is cool, ' + myName;
+  }
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('name');
+  myHeading.innerHTML = 'Mozilla is cool, ' + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
+
+  
+  
+  
 
