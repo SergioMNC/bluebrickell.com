d3dfe6a251d676506fb8c743575c6b1ae64f69af
Sergio <106384030+SergioMNC@users.noreply.github.com>
2023-11-08 10:56:50 -0500

    ok

--- scripts/main.js
+++ scripts/main.js
@@ -1,26 +1,3 @@
-
-/*
-let myButton = document.querySelector("button");
-let myHeading = document.querySelector("h1");
-
-function setUserName() {
-    const myName = prompt("Please enter your name.");
-    localStorage.setItem("name", myName);
-    myHeading.textContent = `Poker is great, ${myName}`;   
-  }
-  if (!localStorage.getItem("name")) {
-   setUserName();
-  } else {
-    const storedName = localStorage.getItem("name");
-    myHeading.textContent = `Poker is great, ${storedName}`;
-  }
-
-myButton.onclick = () => {
-  setUserName();
-};
-
-*/
-
 let myImage = document.querySelector("img");
 myImage.onclick = () => {
   let mySrc = myImage.getAttribute("src");
@@ -31,6 +8,7 @@ myImage.onclick = () => {
     myImage.setAttribute("src, images/logo.jpg")
   }
 }
+
 //personalized welcome message
 let myButton = document.querySelector('button');
 let myHeading = document.querySelector('h1');
