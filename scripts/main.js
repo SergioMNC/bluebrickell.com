4229c29b8a1a1297b2e4c1128c0e33139b1f0789
Sergio <106384030+SergioMNC@users.noreply.github.com>
2023-11-22 20:49:46 -0500

    ok

--- scripts/main.js
+++ scripts/main.js
@@ -1,38 +1,4 @@
-let myImage = document.querySelector("img");
-myImage.onclick = () => {
-  let mySrc = myImage.getAttribute("src");
-  if (mySrc === "images/logo.jpg"){
-    myImage.setAttribute("src", "images/OIG.jpg");
-  
-  }else{
-    myImage.setAttribute("src, images/logo.jpg")
-  }
-}
 
-//personalized welcome message
-let myButton = document.querySelector('button');
-let myHeading = document.querySelector('h1');
-
-function setUserName() {
-  let myName = prompt('Please enter your name.');
-  if(!myName) {
-    setUserName();
-  } else {
-    localStorage.setItem('name', myName);
-    myHeading.innerHTML = 'Mozilla is cool, ' + myName;
-  }
-}
-
-if(!localStorage.getItem('name')) {
-  setUserName();
-} else {
-  let storedName = localStorage.getItem('name');
-  myHeading.innerHTML = 'Mozilla is cool, ' + storedName;
-}
-
-myButton.onclick = function() {
-  setUserName();
-}
 
 
 
